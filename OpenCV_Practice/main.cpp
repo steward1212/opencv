e31cd8c9eb6d3e317634bb2e46bf877c771a676a
steward <steward831212@yahoo.com.tw>
2017-01-24 14:44:39 +0800

    show camera

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -1,15 +1,35 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/opencv.hpp"
+#include <iostream>
 
-
+using namespace std;
 using namespace cv;
 
 int main()
 {
-	Mat img = imread("penggan.jpg");
+	/*Mat img = imread("penggan.jpg");
 	Mat resized_img;
 	resize(img, resized_img, Size(img.cols / 8, img.rows / 8));
 	imshow("Hello World!", resized_img);
-	waitKey();
+	waitKey();*/
+	VideoCapture cap(0);
+
+	if (!cap.isOpened())
+		return -1;
+
+	namedWindow("camera", 1);
+	for(;;)
+	{
+		Mat frame;
+		cap >> frame;
+		imshow("camera", frame);
+		int key = waitKey(30);
+		if (key != 255)
+		{
+			break;
+		}
+	}
+	system("pause");
+	return 0;
 }

8938fb74701182e8805404bf4aa9be68c7bed5cf
steward <steward831212@yahoo.com.tw>
2017-01-25 14:55:29 +0800

    show merge edges and origin

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -6,6 +6,23 @@
 using namespace std;
 using namespace cv;
 
+int slider_value;
+Mat frameA;
+Mat frameB;
+
+void on_trackbar(int, void*)
+{
+	double alpha = (double)slider_value / 100.0;
+	double beta = (1.0 - alpha);
+	Mat dst;
+	Mat out;
+	Mat in[] = { frameB, frameB, frameB };
+	merge(in, 3, out);
+
+	addWeighted(frameA, beta, out, alpha, 0.0, dst);
+	imshow("camera", dst);
+}
+
 int main()
 {
 	/*Mat img = imread("penggan.jpg");
@@ -18,14 +35,18 @@ int main()
 	if (!cap.isOpened())
 		return -1;
 
-	namedWindow("camera", 1);
+	namedWindow("camera", CV_WINDOW_KEEPRATIO);
 	for(;;)
 	{
 		Mat frame;
 		Mat edges;
 		cap >> frame;
-		Canny(frame, edges, 10, 100);
-		imshow("camera", edges);
+		Canny(frame, edges, 100, 300);
+		frameA = frame.clone();
+		frameB = edges.clone();
+		createTrackbar("Ratio", "camera", &slider_value, 100, on_trackbar);
+		on_trackbar(slider_value, 0);
+		//imshow("camera", frameA);
 		int key = waitKey(30);
 		if (key != 255)
 		{
